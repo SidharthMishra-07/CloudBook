25c90f1a92f54ff06c9a92b9976576673e13800f
Sidharth Mishra <sidharth1604@gmail.com>
2022-01-19 22:38:15 +0530

    Auth.js Added

--- Backend/index.js
+++ Backend/index.js
@@ -7,9 +7,8 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Sidharth, Welcome')
-})
+//Available Routes
+app.use('/api/auth', require('./routes/auth'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
