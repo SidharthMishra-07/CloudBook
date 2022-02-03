4f7b4d268924199042b5080d60dac2253d060902
Sidharth Mishra <sidharth1604@gmail.com>
2022-02-03 20:15:55 +0530

    Changes Added

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -71,7 +71,7 @@ router.post('/createuser',[
     const errors = validationResult(req);
     if (!errors.isEmpty()) 
     {
-      return res.status(400).json({ errors: errors.array() });  
+      return res.status(400).json({errors: errors.array()});  
     }
 
     const {email,password} = req.body;
