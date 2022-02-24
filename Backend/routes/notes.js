cfb39c1ab2d1604a04ad0aff691f5763cc5135f6
Sidharth Mishra <sidharth1604@gmail.com>
2022-02-24 19:08:17 +0530

    Route Added

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -11,9 +11,26 @@ router.get('/fetchnotes', fetchUser, async (req, res) => {
   })
 
 //ROUTE 2: Fetch new notes from the User using POST "/api/notes/fetchnotes".
-router.get('/fetchnotes', fetchUser, async (req, res) => {
-    const notes = await Notes.find({ user: req.user.id });
-    res.json(notes);
+router.post('/addnote', fetchUser,[
+    body('title','Enter a valid title').isLength({ min: 3 }),
+    body('description','Description must be atleast 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+    const {title,description} = req.body;
+
+    //If there are errors then return 400 bad request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) 
+    {
+      return res.status(400).json({ errors: errors.array() });  
+    }
+    const note = new Note({
+      title,
+      description,
+      user: req.user.id
+    })
+    note.save()
+
+    res.json(notes)
   })
 
   module.exports = router;
