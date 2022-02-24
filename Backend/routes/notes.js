345c874b0dfc4e27745cbdb48417c6f4f0987eb5
Sidharth Mishra <sidharth1604@gmail.com>
2022-02-24 19:13:28 +0530

    TryCatch

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -6,31 +6,42 @@ const { body, validationResult } = require('express-validator');
 
 //ROUTE 1: Fetch all notes from the User using GET "/api/notes/fetchnotes".
 router.get('/fetchnotes', fetchUser, async (req, res) => {
+  try {
     const notes = await Notes.find({ user: req.user.id });
     res.json(notes);
-  })
+  }
+  catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+})
 
 //ROUTE 2: Fetch new notes from the User using POST "/api/notes/fetchnotes".
-router.post('/addnote', fetchUser,[
-    body('title','Enter a valid title').isLength({ min: 3 }),
-    body('description','Description must be atleast 5 characters').isLength({ min: 5 }),
+router.post('/addnote', fetchUser, [
+  body('title', 'Enter a valid title').isLength({ min: 3 }),
+  body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),
 ], async (req, res) => {
-    const {title,description} = req.body;
+  try {
+    const { title, description } = req.body;
 
     //If there are errors then return 400 bad request
     const errors = validationResult(req);
-    if (!errors.isEmpty()) 
-    {
-      return res.status(400).json({ errors: errors.array() });  
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
     const note = new Note({
       title,
       description,
       user: req.user.id
     })
-    note.save()
+    const savedNote = await note.save()
 
-    res.json(notes)
-  })
+    res.json(savedNote)
+  }
+  catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+})
 
-  module.exports = router;
+module.exports = router;
