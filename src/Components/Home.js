4a911c5163bb43e6be363a61d0c6e8704616903e
Sidharth Mishra <sidharth1604@gmail.com>
2022-07-06 03:26:11 +0530

    Changes Added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,4 @@
-import React, {useContext} from 'react'
-import noteContext from '../Context/notes/noteContext'
+import React from 'react'
 
 export const Home = () => {
   return (
--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import noteContext from '../Context/notes/noteContext'
 
 export const Notes = () => {
     const context = useContext(noteContext);
