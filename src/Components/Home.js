d6feca9356798a4440f154957e87b71ca4ca74d8
Sidharth Mishra <sidharth1604@gmail.com>
2022-07-14 15:27:37 +0530

    Changes Added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,12 +8,12 @@ export const Home = () => {
         <h2>Add your Post</h2>
         <form className='my-3'>
           <div className="form-group my-2">
-            <label for="exampleInputEmail1">Email address</label>
+            <label htmlFor="exampleInputEmail1">Email address</label>
             <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
             <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
           </div>
           <div className="form-group my-2">
-            <label for="exampleInputPassword1">Password</label>
+            <label htmlFor="exampleInputPassword1">Password</label>
             <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
           </div>
           <button type="submit" className="btn btn-primary my-3">Submit</button>
