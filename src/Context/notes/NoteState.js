b28539209d4a10f1f0d8fa1d63c4f08f1a2d2b49
Sidharth Mishra <sidharth1604@gmail.com>
2022-07-15 17:23:54 +0530

    Changes Made

--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -5,58 +5,21 @@ import { useState } from "react";
 const NoteState = (props)=>{
     const host = "http://localhost:5000";
     
-    const notesInitial = [
-        {
-            "_id": "62178c574272a4eddb138d66",
-            "user": "61ec21c527c3783f5a2592d8",
-            "title": "Second Note12",
-            "description": "This is the second note12",
-            "date": "2022-02-24T13:47:03.726Z",
-            "__v": 0
-        },
-        {
-            "_id": "62178c574272a4eddb138d66",
-            "user": "61ec21c527c3783f5a2592d8",
-            "title": "Second Note12",
-            "description": "This is the second note12",
-            "date": "2022-02-24T13:47:03.726Z",
-            "__v": 0
-        },
-        {
-            "_id": "62178c574272a4eddb138d66",
-            "user": "61ec21c527c3783f5a2592d8",
-            "title": "Second Note12",
-            "description": "This is the second note12",
-            "date": "2022-02-24T13:47:03.726Z",
-            "__v": 0
-        },
-        {
-            "_id": "62178c574272a4eddb138d66",
-            "user": "61ec21c527c3783f5a2592d8",
-            "title": "Second Note12",
-            "description": "This is the second note12",
-            "date": "2022-02-24T13:47:03.726Z",
-            "__v": 0
-        },
-        {
-            "_id": "62178c574272a4eddb138d66",
-            "user": "61ec21c527c3783f5a2592d8",
-            "title": "Second Note12",
-            "description": "This is the second note12",
-            "date": "2022-02-24T13:47:03.726Z",
-            "__v": 0
-        },
-        {
-            "_id": "62178c574272a4eddb138d66",
-            "user": "61ec21c527c3783f5a2592d8",
-            "title": "Second Note12",
-            "description": "This is the second note12",
-            "date": "2022-02-24T13:47:03.726Z",
-            "__v": 0
-        },
-    ]
-
+    const notesInitial = [];
     const [notes, setNotes] = useState(notesInitial);
+
+    //get all notes
+    const getNotes = async () => {
+        const response = await fetch(`${host}/api/notes/fetchnotes`,{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjFlYzIxYzUyN2MzNzgzZjVhMjU5MmQ4In0sImlhdCI6MTY0MzExODA1OX0.ypv293phzAEziKcEb-omrb6HY_Cf2xnQLWebg997zhA'
+          },
+        });
+        const json = await response.json();
+        console.log(json);
+    }
 
     //Add a note
     const addnote = async (title, link, description, date) => {
