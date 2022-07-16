9d229023fe38833a50c91bf2b72157352bdde5aa
Sidharth Mishra <sidharth1604@gmail.com>
2022-07-16 11:35:05 +0530

    Modal Added

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -1,6 +1,7 @@
 import React, {useContext, useEffect} from 'react'
 import noteContext from '../Context/notes/noteContext'
 import { AddNote } from './AddNote';
+import Modal from './Modal';
 import NoteItem from './NoteItem';
 
 export const Notes = () => {
@@ -10,14 +11,17 @@ export const Notes = () => {
         getNotes();
         //eslint-disable-next-line
     }, [])
+    const updateNote = (note) => {
 
+    }
     return (
         <>
         <AddNote/>
+        <Modal/>
         <div className="row my-3">
             <h2>Your Post</h2>
             {notes.map((note) => {
-                return <NoteItem key={note._id} note={note}/>
+                return <NoteItem key={note._id} updateNote={updateNote} note={note}/>
             })}
         </div>
         </>
