c3d9983afc61d7c7874dfcee8a7a0c1d82bda65a
Sidharth Mishra <sidharth1604@gmail.com>
2022-07-16 20:56:23 +0530

    Login updated

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,50 +1,62 @@
-import React, {useState} from 'react'
-import { useHistory } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useHistory, Link } from 'react-router-dom'
+import './Login.css'
 
 const Login = (props) => {
-    const [credentials, setCredentials] = useState({email: "", password: ""})
+    const [credentials, setCredentials] = useState({ email: "", password: "" })
     let history = useHistory();
 
-    const handleSubmit = async(e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:5000/api/auth/login",{
+        const response = await fetch("http://localhost:5000/api/auth/login", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({email: credentials.email, password: credentials.password})
-            });
-            const json = await response.json();
-            console.log(json);
+            },
+            body: JSON.stringify({ email: credentials.email, password: credentials.password })
+        });
+        const json = await response.json();
+        console.log(json);
 
-            if (json.success){
-                // Save the auth token and redirect
-                localStorage.setItem('token', json.authtoken); 
-                history.push("/");
-            }
-            else{
-                alert("Invalid credentials");
-            }
+        if (json.success) {
+            // Save the auth token and redirect
+            localStorage.setItem('token', json.authtoken);
+            history.push("/");
+        }
+        else {
+            alert("Invalid credentials");
+        }
     }
-    const onChange = (e)=>{
-        setCredentials({...credentials, [e.target.name]: e.target.value})
+    const onChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value })
     }
 
 
     return (
-        <div className='container my-5'>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" value={credentials.email} id="email" name="email" onChange={onChange}aria-describedby="emailHelp" required/>
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        <div className='global-container'>
+            <div className="card login-form">
+                <div className="card-body">
+                    <h1 className="card-title text-center">Login</h1>
+                    <div className="card-text">
+                        <form onSubmit={handleSubmit}>
+                            <div className="form-group">
+                                <label htmlFor="email" className="form-label">Email address</label>
+                                <input type="email" className="form-control" value={credentials.email} id="email" name="email" onChange={onChange} aria-describedby="emailHelp" required />
+                                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password" className="form-label">Password</label>
+                                <input type="password" className="form-control" value={credentials.password} id="password" name="password" onChange={onChange} required />
+                            </div>
+                            <button type="submit" className="btn btn-primary btn-block my-3">Sign In</button>
+
+                            <div className="signup">
+                                <p>Don't have an account? <Link to ="/signup">Sign up</Link></p>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" value={credentials.password} id="password" name="password" onChange={onChange} required/>
-                </div>
-                <button type="submit" className="btn btn-primary my-3">Submit</button>
-            </form>
+            </div>
         </div>
     )
 }
